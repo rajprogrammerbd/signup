cb067e5a9b9d925a7706cdf0613052ccc0b5c8db
Raj Dutta <rd2249619@gmail.com>
2020-09-10 10:56:18 +0600

    Updates

---
+++ precache-manifest.12c520e01d381a4e614252db30f185ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0114ed35aa5e252e593291e2437b741a",
+    "url": "/signup/index.html"
+  },
+  {
+    "revision": "b1b95328a5f627eab2d7",
+    "url": "/signup/static/css/main.28f7a2ed.chunk.css"
+  },
+  {
+    "revision": "afee8537b86a64217402",
+    "url": "/signup/static/js/2.2bef0b7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/signup/static/js/2.2bef0b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b95328a5f627eab2d7",
+    "url": "/signup/static/js/main.de3124a9.chunk.js"
+  },
+  {
+    "revision": "3d0725078ce9e3a1e09a",
+    "url": "/signup/static/js/runtime-main.292f577f.js"
+  }
+]);
